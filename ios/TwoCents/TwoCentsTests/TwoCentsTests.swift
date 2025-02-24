8471844afa3348f91c919cd2b55fa1dbf492f635
eliu22 <gel8846@nyu.edu>
2025-02-24 17:37:30 -0500

    Intial Commit

---
+++ ios/TwoCents/TwoCentsTests/TwoCentsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TwoCentsTests.swift
+//  TwoCentsTests
+//
+//  Created by Eric Liu on 2025/2/24.
+//
+
+import Testing
+@testable import TwoCents
+
+struct TwoCentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
