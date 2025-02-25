8fe39a83fbe666ff6e19da5d38a970354b114635
ericliu22 <ericliu108@gmail.com>
2025-02-25 01:32:21 -0500

    Working reverseproxy

--- api/db/initdb/init.sql
+++ api/db/initdb/init.sql
@@ -1,13 +1,3 @@
--- 1. Create a new user with a password
-CREATE USER admin WITH PASSWORD '';
-
--- 2. Create a new database owned by that user
-CREATE DATABASE api OWNER admin;
-
--- 3. (Optional) Grant all privileges on the new DB to the user
-GRANT ALL PRIVILEGES ON DATABASE api TO admin;
-
--- 4. Connect to the new database
 \connect api;
 
 -- 5. (Optional) Create extensions, e.g. for UUID generation
