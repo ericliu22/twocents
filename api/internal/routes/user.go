96e28fba4a1c09eae4c00674e4df221591ff4097
ericliu22 <ericliu108@gmail.com>
2025-02-26 23:30:16 -0500

    Mistyped a function

--- api/internal/routes/user.go
+++ api/internal/routes/user.go
@@ -12,5 +12,5 @@ import (
 func SetupUserRoutes(router *gin.RouterGroup, queries *database.Queries, authClient *auth.Client) {
 	r := router.Group("/user", middleware.AuthMiddleware(authClient))
 	r.GET("/get-user", handlers.GetUserHandler(queries))
-	r.POST("/register-user", handlers.GetUserHandler(queries))
+	r.POST("/register-user", handlers.RegisterUserHandler(queries))
 }
