b6c3020ef0f5919251ad42c4739549cb8972833a
ericliu22 <ericliu108@gmail.com>
2025-03-15 01:40:24 -0400

    Updated upload link method

--- api/internal/handlers/post/upload_link_post.go
+++ api/internal/handlers/post/upload_link_post.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	database "api/internal/core/db"
 	"api/internal/middleware"
-	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,59 +10,27 @@ import (
 )
 
 type Link struct {
+	PostId uuid.UUID `json:"postId"`
 	MediaUrl string `json:"mediaUrl"`
 }
 
 func UploadLinkPostHandler(queries *database.Queries) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		token, tokenErr := middleware.GetAuthToken(ctx)
+		_, tokenErr := middleware.GetAuthToken(ctx)
 		if tokenErr != nil {
 			ctx.String(http.StatusUnauthorized, "Unauthorized")
 			gin.DefaultWriter.Write([]byte("Unauthorized"))
 			return
 		}
-		user, userErr := queries.GetFirebaseId(ctx.Request.Context(), token.UID)
-		if userErr != nil {
-			ctx.String(http.StatusInternalServerError, "Failed to fetch user")
-			gin.DefaultWriter.Write([]byte("Failed to fetch user" + userErr.Error()))
-			return
-		}
-		postJSON := ctx.PostForm("post")
-		if postJSON == "" {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "post part of form is empty"})
-			gin.DefaultWriter.Write([]byte("Post form is empty"))
-			return
-		}
-
-		var post database.Post
-		if err := json.Unmarshal([]byte(postJSON), &post); err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Error parsing JSON: " + err.Error()})
-			gin.DefaultWriter.Write([]byte("Request body not as specified: " + err.Error()))
-			return
-		}
-		if user.ID != post.UserID {
-			ctx.String(http.StatusUnauthorized, "Unauthorized")
-			gin.DefaultWriter.Write([]byte("Unauthorized"))
-			return
-		}
-
-		linkJSON := ctx.PostForm("link")
-		if linkJSON == "" {
-			ctx.String(http.StatusBadRequest, "Failed to get form file")
-			gin.DefaultWriter.Write([]byte("File form is empty"))
-			return
-		}
-
 		var linkRequest Link
-		if err := json.Unmarshal([]byte(linkJSON), &linkRequest); err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Error parsing JSON: " + err.Error()})
-			gin.DefaultWriter.Write([]byte("Request body not as specified: " + err.Error()))
-			return
+		if bindErr := ctx.ShouldBindJSON(&linkRequest); bindErr != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Request body not as specified"})
+			gin.DefaultWriter.Write([]byte("Request body not as specified: " + bindErr.Error()))
 		}
 
 		linkParams := database.CreateLinkParams{
 			ID:       uuid.New(),
-			PostID:   post.ID,
+			PostID:   linkRequest.PostId,
 			MediaUrl: linkRequest.MediaUrl,
 		}
 
