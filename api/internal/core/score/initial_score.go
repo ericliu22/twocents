5d0cb906cfc5faf625521ab34a00f59ce86d5744
ericliu22 <ericliu108@gmail.com>
2025-04-03 23:07:51 -0400

    asdfasdf

--- api/internal/core/score/initial_score.go
+++ api/internal/core/score/initial_score.go
@@ -10,12 +10,13 @@ import (
 
 func InitialScore(queries *database.Queries) {
 	friendGroups, err := queries.ListFriendGroups(context.Background())
-	log.Printf("Fetched friend groups for calculation job")
 	if err != nil {
 		log.Printf("Failed to fetch friend groups for calculation job")
 		gin.DefaultWriter.Write([]byte("Failed to fetch friend groups for calculation job"))
 		return
 	}
+	log.Printf("Fetched friend groups for calculation job")
+	log.Printf("Friend groups: %v", friendGroups)
 	for _, friendGroup := range friendGroups {
 		go RoutineScoreCalculator(friendGroup.ID, queries)
 	}
