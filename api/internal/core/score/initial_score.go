cffaef98444d7363270be8c2194b068162057b48
ericliu22 <ericliu108@gmail.com>
2025-04-03 22:48:18 -0400

    Update initial_score.go

--- api/internal/core/score/initial_score.go
+++ api/internal/core/score/initial_score.go
@@ -4,15 +4,19 @@ import (
 	database "api/internal/core/db"
 	"context"
 	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 func InitialScore(queries *database.Queries) {
 	friendGroups, err := queries.ListFriendGroups(context.Background())
 	if err != nil {
 		log.Printf("Failed to fetch friend groups for calculation job")
+		gin.DefaultWriter.Write([]byte("Failed to fetch friend groups for calculation job"))
 		return
 	}
 	for _, friendGroup := range friendGroups {
 		go RoutineScoreCalculator(friendGroup.ID, queries)
 	}
+	gin.DefaultWriter.Write([]byte("Ran initial score"))
 }
