75ec3a1eafdc2fe5adf3cc285a8e1ed1999d5bbf
ericliu22 <ericliu108@gmail.com>
2025-04-07 13:18:22 -0400

    logging

--- api/internal/handlers/user/register_device_token.go
+++ api/internal/handlers/user/register_device_token.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	database "api/internal/core/db"
 	"api/internal/middleware"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -17,6 +18,8 @@ func RegisterDeviceTokenHandler(queries *database.Queries) gin.HandlerFunc {
 		token, tokenErr := middleware.GetAuthToken(ctx)
 		if tokenErr != nil {
 			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			gin.DefaultWriter.Write([]byte("Unauthorized: " + tokenErr.Error()))
+			log.Println("Unauthorized: " + tokenErr.Error())
 			return
 		}
 
@@ -24,12 +27,15 @@ func RegisterDeviceTokenHandler(queries *database.Queries) gin.HandlerFunc {
 		if userErr != nil {
 			ctx.String(http.StatusInternalServerError, "Failed to fetch user: "+userErr.Error())
 			gin.DefaultWriter.Write([]byte("Failed to fetch user: " + userErr.Error()))
+			log.Println("Failed to fetch user: " + userErr.Error())
 			return
 		}
 
 		var registerDeviceTokenRequest RegisterDeviceTokenRequest
 		if bindErr := ctx.Bind(&registerDeviceTokenRequest); bindErr != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Request body not as specified"})
+			gin.DefaultWriter.Write([]byte("Request body not as specified: " + bindErr.Error()))
+			log.Println("Request body not as specified: " + bindErr.Error())
 			return
 		}
 
@@ -40,6 +46,8 @@ func RegisterDeviceTokenHandler(queries *database.Queries) gin.HandlerFunc {
 		err := queries.AddDeviceToken(ctx.Request.Context(), addToken)
 		if err != nil {
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to insert device token"})
+			log.Println("Failed to insert device token: " + err.Error())
+			gin.DefaultWriter.Write([]byte("Failed to insert device token: " + err.Error()))
 			return
 		}
 
